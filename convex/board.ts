d721314d14a1878f9a61d358f6f6bb4d80ec336e
priyanshu <priyanshumittal0916@gmail.com>
2024-02-11 10:13:18 +0530

    final commit

--- convex/board.ts
+++ convex/board.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import {mutation} from "./_generated/server";
+import {mutation,query} from "./_generated/server";
 
 const images = [
     "/placeholders/1.svg",
@@ -146,6 +146,14 @@ export const unfavourite = mutation({
             throw new Error("favourited board not Found");
         }
         await ctx.db.delete(existingFavourite._id);
+        return board;
+    }
+})
+
+export const get = query({
+    args:{id:v.id("boards")},
+    handler:async (ctx,args)=>{
+        const board = ctx.db.get(args.id);
         return board;
     }
 })
